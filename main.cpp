3a215ad84fb97bdeb5ef98d400c51b74e3f7f369
drshaggy <connorhughes@fastmail.com>
2025-12-06 19:32:34 +0000

    changed print logic

--- main.cpp
+++ main.cpp
@@ -16,8 +16,7 @@ int main() {
     while(true)
     {
         char key = static_cast<char>(keyboard.read());
-        if (key)
-            std::cout << "0: "<< key;
+        std::cout << "0: "<< key;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
