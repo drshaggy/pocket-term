3930d0e20fa6c4757830c62b37439205923f44d9
drshaggy <connorhughes@fastmail.com>
2025-12-06 19:56:52 +0000

    change baud rate

--- drivers/cardkb.cpp
+++ drivers/cardkb.cpp
@@ -20,7 +20,7 @@ int CardKB::initialise()
     std::clog << "I2C Operations started" << std::endl
 			  << "Setting slave address and baud rate" << std::endl;
     bcm2835_i2c_setSlaveAddress(m_address);  //I2C address
-    bcm2835_i2c_set_baudrate(10000);    //1M baudrate
+    bcm2835_i2c_set_baudrate(100000);    //1M baudrate
     std::clog << "Address and baud rate set" << std::endl;
     return 0;
 };
@@ -28,6 +28,7 @@ int CardKB::initialise()
 int CardKB::read()
 {
     //std::clog << "Attempting to read keyboard input" << std::endl;
+    m_buf[0] = 0
     uint32_t len = 1;
     bcm2835_i2c_read(m_buf, len);
     return m_buf[0];
