c642cc686d5c1c4bfd81413dab770684f19686e1
drshaggy <connorhughes@fastmail.com>
2025-12-08 18:30:19 +0000

    changed time format

--- utilities.cpp
+++ utilities.cpp
@@ -4,6 +4,6 @@ std::string getCurrentTime() {
     std::time_t t = std::time(nullptr);
     std::tm tm = *std::localtime(&t);
     std::stringstream ss;
-    ss << std::put_time(&tm, "%c %Z");
+    ss << std::put_time(&tm, "%H:%M");
     return ss.str();
 }
