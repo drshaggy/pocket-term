4ba15559d69be889075657cf7684c3960448d94b
drshaggy <connorhughes@fastmail.com>
2025-12-14 20:39:03 +0000

    Removed bitmap and writing text instead

--- src/drivers/waveshare_eink.cpp
+++ src/drivers/waveshare_eink.cpp
@@ -16,20 +16,18 @@ WaveshareEink::WaveshareEink(bool verticalOrientation) : Display(verticalOrienta
         DEV_Delay_ms(500);
 
         UBYTE *BlackImage;
-        UDOUBLE Imagesize = ((EPD_4in26_HEIGHT % 8 == 0)? (EPD_4in26_HEIGHT / 8 )
-                             : (EPD_4in26_HEIGHT / 8 + 1)) * EPD_4in26_WIDTH;
+        UDOUBLE Imagesize = ((EPD_4in26_WIDTH % 8 == 0)? (EPD_4in26_WIDTH / 8 )
+                             : (EPD_4in26_WIDTH / 8 + 1)) * EPD_4in26_HEIGHT;
         if((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
              spdlog::error("Failed to apply for black memory...\r\n");
         }
-        Paint_NewImage(BlackImage, EPD_4in26_HEIGHT, EPD_4in26_WIDTH, ROTATE_90, WHITE);
+        printf("Paint_NewImage\r\n");
+        Paint_NewImage(BlackImage, EPD_4in26_WIDTH, EPD_4in26_HEIGHT, 0, WHITE);
         Paint_SelectImage(BlackImage);
         Paint_Clear(WHITE);
-        if (GUI_ReadBmp("resources/PocketTermSplash.bmp", 0, 0) != 0) {
-            spdlog::error("Failed to read Splashscreen Bitmap");
-        } else {
-            EPD_4in26_Display(BlackImage);
-            DEV_Delay_ms(2000);
-        }
+        Paint_DrawString_EN(10, 20, "Pocket Term", &Font12, WHITE, BLACK);
+        EPD_4in26_Display(BlackImage);
+        DEV_Delay_ms(2000);
         spdlog::info("Waveshare Eink Device initialised");
     }
 
