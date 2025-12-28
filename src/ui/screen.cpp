eda94f1cb2867672c1c691a05c5acf8301148af8
drshaggy <connorhughes@fastmail.com>
2025-12-28 19:20:00 +0000

    fixed null check in copy constructor

--- src/ui/screen.cpp
+++ src/ui/screen.cpp
@@ -4,8 +4,13 @@ Screen::Screen(uint8_t width, uint8_t height)
 	: m_width{width}, m_height{height}
 {};
 
-Screen::Screen(const Screen& other) {
-    m_rootWidget = other.m_rootWidget->clone();
+Screen::Screen(const Screen& other)
+    : m_width(other.m_width),
+      m_height(other.m_height)
+{
+    if (other.m_rootWidget) {
+        m_rootWidget = other.m_rootWidget->clone();
+    }
 }
 
 Screen& Screen::operator=(const Screen& other) {
